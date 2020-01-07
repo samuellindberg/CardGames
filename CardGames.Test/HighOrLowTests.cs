ac289d2853b3d5b2792cef5f717766b8ce7e6bd2
Samuel Lindberg <lindberg.samuel@gmail.com>
2020-01-07 08:55:16 +0100

    Splitted test method on High or low

--- CardGames.Test/HighOrLowTests.cs
+++ CardGames.Test/HighOrLowTests.cs
@@ -13,39 +13,64 @@ namespace CardGames.Test
         HighOrLowCore c = new HighOrLowCore(new HighScoreServices(new CardGamesContext()));
 
         [TestMethod]
-        public void CheckIfHighOrLowTests()
+        public void HeartsFourHigherThanDiamondsFour()
         {
-            c.OpenCard = new PlayingCard(CardRank.Eight, CardSuit.Hearts, 'x', "x");
-            c.PlayersCard = new PlayingCard(CardRank.Two, CardSuit.Diamonds, 'x', "x");
-            c.CheckHighOrLow();
-
-            Assert.IsFalse(c.high);
-
-            c.OpenCard = new PlayingCard(CardRank.Three, CardSuit.Spades, 'x', "x");
-            c.PlayersCard = new PlayingCard(CardRank.Queen, CardSuit.Clubs, 'x', "x");
+            c.OpenCard = new PlayingCard(CardRank.Four, CardSuit.Diamonds, 'x', "x");
+            c.PlayersCard = new PlayingCard(CardRank.Four, CardSuit.Hearts, 'x', "x");
             c.CheckHighOrLow();
 
             Assert.IsTrue(c.high);
+        }
 
-            // Ace higher than king
-            c.OpenCard = new PlayingCard(CardRank.King, CardSuit.Spades, 'x', "x");
-            c.PlayersCard = new PlayingCard(CardRank.Ace, CardSuit.Clubs, 'x', "x");
-            c.CheckHighOrLow();
-
-            Assert.IsTrue(c.high);
-
+        [TestMethod]
+        public void ClubsTenLowerThanSpadesTen()
+        {
             //Testing suit ranking
             c.OpenCard = new PlayingCard(CardRank.Ten, CardSuit.Spades, 'x', "x");
             c.PlayersCard = new PlayingCard(CardRank.Ten, CardSuit.Clubs, 'x', "x");
             c.CheckHighOrLow();
 
             Assert.IsFalse(c.high);
+        }
 
-            c.OpenCard = new PlayingCard(CardRank.Four, CardSuit.Diamonds, 'x', "x");
-            c.PlayersCard = new PlayingCard(CardRank.Four, CardSuit.Hearts, 'x', "x");
+        [TestMethod]
+        public void ClubsQueenLowerThanHeartsAce()
+        {
+            c.OpenCard = new PlayingCard(CardRank.Ace, CardSuit.Hearts, 'x', "x");
+            c.PlayersCard = new PlayingCard(CardRank.Queen, CardSuit.Clubs, 'x', "x");
+            c.CheckHighOrLow();
+
+            Assert.IsFalse(c.high);
+        }
+
+        [TestMethod]
+        public void ClubsAceHigherThanSpadesKing()
+        {
+            c.OpenCard = new PlayingCard(CardRank.King, CardSuit.Spades, 'x', "x");
+            c.PlayersCard = new PlayingCard(CardRank.Ace, CardSuit.Clubs, 'x', "x");
             c.CheckHighOrLow();
 
             Assert.IsTrue(c.high);
+        }
+
+        [TestMethod]
+        public void ClubsQueenHigherThanSpadesThree()
+        {
+            c.OpenCard = new PlayingCard(CardRank.Three, CardSuit.Spades, 'x', "x");
+            c.PlayersCard = new PlayingCard(CardRank.Queen, CardSuit.Clubs, 'x', "x");
+            c.CheckHighOrLow();
+
+            Assert.IsTrue(c.high);
+        }
+
+        [TestMethod]
+        public void Diamond2LowerThanHearts8()
+        {
+            c.OpenCard = new PlayingCard(CardRank.Eight, CardSuit.Hearts, 'x', "x");
+            c.PlayersCard = new PlayingCard(CardRank.Two, CardSuit.Diamonds, 'x', "x");
+            c.CheckHighOrLow();
+
+            Assert.IsFalse(c.high);
         }
     }
 }
